c08ef856e51bb7587191519d2039f37819465154
josevenzke <josevenzkejr@gmail.com>
2021-09-18 21:48:18 -0300

    impoting and using test component

--- librario/src/App.vue
+++ librario/src/App.vue
@@ -1,15 +1,14 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <TestLists />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import TestLists from './components/TestLists.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    TestLists
   }
 }
 </script>
