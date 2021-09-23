fb54d82e3037244859a0e34eaaa32df8a9107583
josevenzke <josevenzkejr@gmail.com>
2021-09-22 23:40:39 -0300

    login in and storing jwt token in memory

--- librario/src/components/Login.vue
+++ librario/src/components/Login.vue
@@ -1,10 +1,32 @@
 <template>
-    
+    <div>
+        <h1>Login:</h1>
+        <input v-model="login" type="text">
+        <input v-model="senha" type="text">
+
+        <button @click="loginToken()">Login</button>
+    </div>
 </template>
 
 <script>
+import axios from 'axios'
 export default {
-    
+    name:'Test',
+    data(){
+        return{
+            login: '',
+            senha:'',
+            userToken: false,
+        }
+    },
+
+    methods:{
+        async loginToken(){
+            const x = await axios.post('http://127.0.0.1:8000/api-token/',{username:this.login, password:this.senha})
+            console.log(x)
+            this.userToken = x.data.token
+        },
+    }
 }
 </script>
 
