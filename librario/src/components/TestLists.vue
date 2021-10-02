776d829658525b7c8a28c57253691cde4476a806
josevenzke <josevenzkejr@gmail.com>
2021-10-01 21:08:58 -0300

    removed unused functions

--- librario/src/components/TestLists.vue
+++ librario/src/components/TestLists.vue
@@ -1,11 +1,5 @@
 <template>
     <div>
-        <h1>Login:</h1>
-        <input v-model="login" type="text">
-        <input v-model="senha" type="text">
-
-        <button @click="loginToken()">Login</button>
-
         <h1>Livros:</h1><button @click="teste()">Mostrar</button>
         <ul>
             <li v-for="book in booksList" :key="book">{{book}}</li>
@@ -19,19 +13,11 @@ export default {
     name:'Test',
     data(){
         return{
-            booksList: false,
-            login: '',
-            senha:'',
-            userToken: false,
+            booksList: [],
         }
     },
 
     methods:{
-        async loginToken(){
-            const x = await axios.post('http://127.0.0.1:8000/api/token/',{username:this.login, password:this.senha})
-            console.log(x)
-            this.userToken = x.data.access
-        },
         async teste(){
             let config = {
                 headers: {
