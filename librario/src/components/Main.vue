ec421c3e11edcb986b6a2697245b399d5767c463
josevenzke <josevenzkejr@gmail.com>
2021-10-02 17:51:38 -0300

    new main component

---
+++ librario/src/components/Main.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+        <div id="nav">
+            <router-link to="/login">Login</router-link> |
+            <router-link to="/test">Test</router-link>
+        </div>
+        <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+    name:'Main'
+}
+</script>
