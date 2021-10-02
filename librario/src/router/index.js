0cdcbefb83b8e200e09cde1e8749274d9a64ec4f
josevenzke <josevenzkejr@gmail.com>
2021-10-02 17:51:26 -0300

    new main route

--- librario/src/router/index.js
+++ librario/src/router/index.js
@@ -1,12 +1,18 @@
 import { createWebHistory, createRouter } from "vue-router";
 import Login from "../components/Login.vue";
 import TestLists from "../components/TestLists.vue";
+import Main from '../components/Main.vue'
 
 const routes = [
   {
     path: "/login",
     name: "Login",
     component: Login,
+  },
+  {
+    path:"/main",
+    name:"Main",
+    component: Main,
   },
   {
     path: "/test",
