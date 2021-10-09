97c1d36a5db2aadefa1fe9b32434ca7c92c71264
josevenzke <josevenzkejr@gmail.com>
2021-10-09 00:09:09 -0300

    added vuetify

--- librario/src/App.vue
+++ librario/src/App.vue
@@ -1,23 +1,18 @@
 <template>
-  <Login />
+  <v-app>
+    <v-main>
+      <router-view/>
+    </v-main>
+  </v-app>
 </template>
 
 <script>
-import Login from './components/Login.vue'
+
 export default {
   name: 'App',
-  components:{Login}
-}
 
+  data: () => ({
+    //
+  }),
+}
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
